99e796c671b2f166bf52aa8abe60c72dcf3c99b7
Wajeeh ul Hassan <wajeeh313@hotmail.com>
2012-10-20 04:48:25 -0700

    a bit modified

--- wajeeh programs/question 1.cpp
+++ wajeeh programs/question 1.cpp
@@ -22,19 +22,18 @@ int main()
 
 			if ( a == 'S' || a == 's')
 				{
-					cout << "YOU ARE A STUDENT" << endl;
+					
 					cout << "For male press M" << endl;
 					cout << "For female press F" << endl;
 					cin >> a;
 					if ( a == 'M' || a == 'm')
 						{
-							cout << "YOU ARE A MALE STUDENT" << endl;
 							cout << "For Graduate press G" << endl;
 							cout << "For Masters press M" << endl;
 							cin >> a;
 							if ( a == 'M' || a == 'm')
 								{
-									cout << "YOU ARE A MALE MASTERS STUDENT" << endl;
+									
 									cout << "For Computer Science press C" << endl;
 									cout << "For Electrical press E" << endl;
 									cout << "For Buisness Administration press B" << endl;
@@ -59,7 +58,7 @@ int main()
 								}
 							else if ( a == 'G' || a == 'g')
 								{
-									cout << "YOU ARE A MALE GRADUATE STUDENT" << endl;
+									
 									cout << "For Computer Science press C" << endl;
 									cout << "For Electrical press E" << endl;
 									cout << "For Buisness Administration press B" << endl;
@@ -90,13 +89,12 @@ int main()
 						}
 					else if ( a == 'F' || a == 'f')
 						{
-							cout << "YOU ARE A FEMALE STUDENT" << endl;
+							
 							cout << "For Graduate press G" << endl;
 							cout << "For Masters press M" << endl;
 							cin >> a;
 							if ( a == 'M' || a == 'm')
 								{
-									cout << "YOU ARE A FEMALE MASTERS STUDENT" << endl;
 									cout << "For Computer Science press C" << endl;
 									cout << "For Electrical press E" << endl;
 									cout << "For Buisness Administration press B" << endl;
@@ -121,7 +119,6 @@ int main()
 								}
 							else if ( a == 'G' || a == 'g')
 								{
-									cout << "YOU ARE A FEMALE GRADUATE STUDENT" << endl;
 									cout << "For Computer Science press C" << endl;
 									cout << "For Electrical press E" << endl;
 									cout << "For Buisness Administration press B" << endl;
@@ -156,19 +153,17 @@ int main()
 				}
 			else if ( a == 'F' || a == 'f')
 				{
-					cout << "YOU ARE A FACULTY MEMBER" << endl;
 					cout << "For Visiting press V" << endl;
 					cout << "For Permanent press P" << endl;
 					cin >> a;
 					if ( a == 'V' || a == 'v')
 						{
-							cout << "YOU ARE A VISITING FACULTY MEMBER" << endl;
 							cout << "For male press M" << endl;
 							cout << "For female press F" << endl;
 							cin >> a;
 							if ( a == 'M' || a == 'm')
 								{
-									cout << "YOU ARE A MALE VISITING FACULTY MEMBER" << endl;
+								
 									cout << "For Computer Science press C" << endl;
 									cout << "For Electrical press E" << endl;
 									cout << "For Buisness Administration press B" << endl;
@@ -193,7 +188,7 @@ int main()
 								}
 							else if ( a == 'F' || a =='f')
 								{
-									cout << "YOU ARE A FEMALE VISITING FACULTY MEMBER" << endl;
+								
 									cout << "For Computer Science press C" << endl;
 									cout << "For Electrical press E" << endl;
 									cout << "For Buisness Administration press B" << endl;
@@ -222,13 +217,12 @@ int main()
 						}
 					else if ( a == 'P' || a == 'p')
 						{
-							cout << "YOU ARE A PERMANENT FACULTY MEMBER" << endl;
 							cout << "For male press M" << endl;
 							cout << "For female press F" << endl;
 							cin >> a;
 							if ( a == 'M' || a == 'm')
 								{
-									cout << "YOU ARE A MALE PERMANENT FACULTY MEMBER" << endl;
+									
 									cout << "For Computer Science press C" << endl;
 									cout << "For Electrical press E" << endl;
 									cout << "For Buisness Administration press B" << endl;
@@ -253,7 +247,6 @@ int main()
 								}
 							else if ( a == 'F' || a =='f')
 								{
-									cout << "YOU ARE A FEMALE VISITING FACULTY MEMBER" << endl;
 									cout << "For Computer Science press C" << endl;
 									cout << "For Electrical press E" << endl;
 									cout << "For Buisness Administration press B" << endl;
@@ -287,14 +280,13 @@ int main()
 				}
 			else if ( a == 'T' || a == 't')
 				{
-					cout << "YOU ARE A MEMBER OF THE STAFF" << endl;
 					cout << "For Maintenance press M" << endl;
 					cout << "For Administration press A" << endl;
 					cout << "For Academincs press D" << endl;
 					cin >> a;
 					if ( a == 'D' || a == 'd' )
 						{
-							cout << "YOU ARE A MEMBER OF THE STAFF IN THE ACADEMINCS DEPARTMENT" << endl;
+							
 							cout << "For Central press C" << endl;
 							cout << "For Department Academics Press A" << endl;
 							cin >> a;
@@ -304,7 +296,7 @@ int main()
 								}
 							else if ( a == 'A' || a == 'a')
 								{
-									cout << "YOU ARE MEMBER OF THE STAFF IN THE DEPARTMENT ACADEMICS" << endl;
+									
 									cout << "For Electrical press E" << endl;
 									cout << "For Computer Science press C" << endl;
 									cout << "For Buisness Administration press B" << endl;
