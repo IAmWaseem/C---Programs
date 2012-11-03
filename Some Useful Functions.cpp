4614628a2383727dfdf6732bd02cb6fe6401f57b
theplaymate <agony.holyamu@hotmail.com>
2012-11-03 21:09:02 +0500

    Some Useful Functions for Project

---
+++ Some Useful Functions.cpp
@@ -0,0 +1,11 @@
+/* 1. Sets output format for bool variables
+   to true and false instead
+   of 1 and 0 */
+
+cout.setf(cout.boolalpha);
+
+/* 2. Unsets output format for bool variables
+   to true and false instead
+   of 1 and 0 */
+
+cout.unsetf(cout.boolalpha);
